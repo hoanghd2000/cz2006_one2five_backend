9bd0f1ed2e27b1cc74fa8cdde9814a9400027826
hoanghd2000 <hoanghd2000@gmail.com>
2022-04-05 11:19:54 +0800

    add ecosystem.config file for pm2 to deploy

---
+++ config/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'one2five_backend',
+      script: './app.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'http://119.8.186.79/',
+        key: '~/.ssh/id_rsa.pub',
+        ref: 'origin/main',
+        repo: 'git@github.com:hoanghd2000/cz2006_one2five_backend.git',
+        path: '/backend',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
